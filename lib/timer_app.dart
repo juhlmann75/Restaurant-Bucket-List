5b9956ff73c1f6f5229b8b356255cf1d83550266
juhlmann75 <34980802+juhlmann75@users.noreply.github.com>
2022-11-13 19:54:54 -0600

    implement timer app

---
+++ lib/timer_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sandbox/timer/timer.dart';
+
+class TimerApp extends StatelessWidget {
+  const TimerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Timer',
+      theme: ThemeData(
+        primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+        colorScheme: const ColorScheme.light(
+          secondary: Color.fromRGBO(72, 74, 126, 1),
+        ),
+      ),
+      home: const TimerPage(),
+    );
+  }
+}
