398a8cc3f6629fd8a55aa5dcc8b009fb4af4900d
juhlmann75 <34980802+juhlmann75@users.noreply.github.com>
2022-11-25 01:32:37 -0600

    implement login bloc

--- lib/login/bloc/login_state.dart
+++ lib/login/bloc/login_state.dart
@@ -1,10 +1,29 @@
 part of 'login_bloc.dart';
 
-abstract class LoginState extends Equatable {
-  const LoginState();
-}
+class LoginState extends Equatable {
 
-class LoginInitial extends LoginState {
+  final FormzStatus status;
+  final Username username;
+  final Password password;
+
+  const LoginState({
+    this.status = FormzStatus.pure,
+    this.username = const Username.pure(),
+    this.password = const Password.pure(),
+  });
+
+  LoginState copyWith({
+    FormzStatus? status,
+    Username? username,
+    Password? password,
+  }) {
+    return LoginState(
+      status: status ?? this.status,
+      username: username ?? this.username,
+      password: password ?? this.password,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [status, username, password];
 }
